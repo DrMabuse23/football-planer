41492e1bca334194a07f0a91858c0e1e6b7816e0
DrMabuse <drmabuse@posteo.de>
2015-12-21 23:11:20 +0100

    add error message item

---
+++ app/auth/component/error-required.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'error-required',
+  template: `
+  <ion-item danger *ngIf="interface.expr">
+    <p danger>{{interface.attr}} ist ein pflicht feld und darf nicht leer sein.</p>
+  </ion-item>`
+})
+
+export class ErrorItemComponent {
+  public interface: ErrorItemInterface;
+  constructor() {
+    console.log('nope');
+    //debugger;
+  }
+}
+
+export interface ErrorItemInterface {
+  expr: any;
+  attr: string;
+}
